7999940e853b1d081a3f1d838adabc59577bfc3c
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-19 13:27:00 -0700

    seperated functions and click listeners

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-// import functions and grab DOM elements
-//addition
-const input1 = document.getElementById('math-input-1');
-const input2 = document.getElementById('math-input-2');
-const addEquals = document.getElementById('add-equals-button');
-const subEquals = document.getElementById('sub-equals-button');
-const multEquals = document.getElementById('mult-equals-button');
-const divEquals = document.getElementById('div-equals-button');
-const result = document.getElementById('math-results');
-// initialize state
-console.log(input1);
-console.log(input2);
+/*import functions*/
+import {
+    functionshere
+} from './utils.js';
 
-// set event listeners to update state and DOM
-//addition function
-addEquals.addEventListener('click', () => {
-    result.textContent = Number(input1.value) + Number(input2.value);
-});
-console.log(result);
-
-//subtraction function
-subEquals.addEventListener('click', () => {
-    result.textContent = Number(input1.value) - Number(input2.value);
-});
-
-//multiplication function
-multEquals.addEventListener('click', () => {
-    result.textContent = Number(input1.value) * Number(input2.value);
-});
-
-//division function
-divEquals.addEventListener('click', () => {
-    result.textContent = Number(input1.value) / Number(input2.value);
-});
+/*make constants for calculations*/
+const add
+const subtract
+const multiply
+const divide
